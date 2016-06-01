1e48e009979e9e104da6c5fb94f56ae8148dd3a0
Brian Cheon <cheonb@amazon.com>
2016-06-01 10:49:37 -0700

    Initial submit for iOS app

---
+++ MiniSnSiOS/MiniSnSiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiniSnSiOS
+//
+//  Created by Cheon, Brian on 6/1/16.
+//  Copyright © 2016 Study Hero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MiniSnSiOS/MiniSnSiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MiniSnSiOS
+//
+//  Created by Cheon, Brian on 6/1/16.
+//  Copyright © 2016 Study Hero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
